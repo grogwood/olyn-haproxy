0e5d2859e8c8b01cc7f68d6e0e631323e3f29c75
Scott Richardson <dev@grogwood.com>
2020-10-10 20:00:42 -0700

    Version bump

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ license 'GPL-3.0'
 supports 'debian', '>= 10'
 source_url 'https://gitlab.com/olyn/olyn_haproxy'
 description 'Installs and configures the Haproxy load balancer'
-version '1.0.3'
+version '2.0.0'
 
 provides 'olyn_haproxy::default'
 recipe 'olyn_haproxy::default', 'Installs Haproxy load balancer'
